95de7a2a3ffcd9729d9170b59e11fc3fd944787b
Gautham <gautham.murthy@imaginea.com>
2019-02-10 22:12:36 +0530

    Lesson4 : Css & Style loaders

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,27 @@
 var path = require('path')
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = {
     entry: './src/js/app.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js',
-        publicPath: '/dist'
-        
+        publicPath: '/dist'   
+    },
+    module : {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            }
+          ]
+    },
+    optimization: {
+        minimizer: [
+            new UglifyJsPlugin()
+        ]
     }
 }
