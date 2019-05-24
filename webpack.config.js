c6740fb949744c68a6bde7355a546e84cf69c493
Gautham <gautham.murthy@imaginea.com>
2019-05-24 13:06:02 +0530

    Dynamic Import setup

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,10 @@
 var path = require('path')
 const CleanWebpackPlugin = require('clean-webpack-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     mode: 'development',
     entry: {
-        index: './src/js/app.js',
+        index: './src/js/index.js',
     },
     output: {
         path: path.resolve(__dirname, 'dist'),
