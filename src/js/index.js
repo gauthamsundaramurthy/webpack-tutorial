63641cf5c801131831476465b578dd15eb947405
Gautham <gautham.murthy@imaginea.com>
2019-05-24 15:42:57 +0530

    Async version of Dynamic Import

--- src/js/index.js
+++ src/js/index.js
@@ -3,13 +3,20 @@
 * below webpackChunkName: "lodash" -->  sets the name of the chunk 
 * for more info, https://webpack.js.org/api/module-methods#import-
 */
+import "@babel/polyfill";
 
-function getComponent() {
-    return import(/* webpackChunkName: "lodash" */ 'lodash').then(({ default: _ }) => {
-        const element = document.createElement('div');
-        element.innerHTML = _.join(['Demo of ','Dynamic Import !!!'], ' ');
-        return element;
-    }).catch(error => 'An error occurred while loading the component');
+async function getComponent() {
+    // const { default: _ } = await import(/* webpackChunkName: "lodash" */ 'lodash');
+    // const element = document.createElement('div');
+    // element.innerHTML = _.join(['Demo of ','Dynamic Import !!!'], ' ');
+    // return element;
+
+   const element = document.createElement('div');
+   const { default: _ } = await import(/* webpackChunkName: "lodash" */ 'lodash');
+
+   element.innerHTML = _.join(['Async version of ', 'Dynamic Import !!!'], ' ');
+
+   return element;
 }
 
 getComponent().then(component => {
